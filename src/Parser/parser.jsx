c977191e5393db0adc9d29681cabb101bb8a740a
wrkid <137333242+wrkid@users.noreply.github.com>
2024-10-23 15:01:28 +0300

    parser by steps

--- src/Parser/parser.jsx
+++ src/Parser/parser.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState } from 'react';
 import * as XLSX from 'xlsx';
 
-import './parser.css';
+import './parser.css'
 
 const markets = {
   title: 'Выберите магазин и платите частями',
@@ -11,11 +11,12 @@ const markets = {
 }
 
 export const Parser = () => {
-  const [ jsonData, setJsonData ] = useState(null);
-  const [ osType, setOsType ] = useState('web')
+  const [jsonData, setJsonData] = useState(null);
+  const [file, setFile] = useState(null);
+  const [osType, setOsType] = useState('web');
 
   const sortLevelTop = (array = []) => {
-    const levelTop = []
+    const levelTop = [];
 
     array.forEach((el) => {
       const market = {
@@ -25,24 +26,24 @@ export const Parser = () => {
         iconFileName: el.iconFileName ?? '',
         description: el.description ?? '',
         webLink: el[`webLink_${osType}`] ?? ''
-      }
+      };
 
-      levelTop.push(market)
-    })
+      levelTop.push(market);
+    });
 
-    return levelTop
-  }
+    return levelTop;
+  };
 
-  const sortLevelAll= (array = []) => {
-    const categoriesMap = new Set()
-    const levelAll = []
+  const sortLevelAll = (array = []) => {
+    const categoriesMap = new Set();
+    const levelAll = [];
 
-    array.forEach((el) => categoriesMap.add(el.category))
+    array.forEach((el) => categoriesMap.add(el.category));
 
     categoriesMap.forEach((category) => {
-      const filteredMarketsByCategory = array.filter((market) => market.category === category)
+      const filteredMarketsByCategory = array.filter((market) => market.category === category);
 
-      const categoryMarkets = []
+      const categoryMarkets = [];
 
       filteredMarketsByCategory.forEach((el) => {
         const market = {
@@ -52,23 +53,28 @@ export const Parser = () => {
           bannerFileName: el.bannerFileName ?? '',
           webLink: el[`webLink_${osType}`] ?? '',
           description: el.description ?? ''
-        }
-  
-        categoryMarkets.push(market)
-      })
+        };
 
-      levelAll.push({category, markets: categoryMarkets})
-    })
+        categoryMarkets.push(market);
+      });
 
-    return levelAll
-  }
+      levelAll.push({ category, markets: categoryMarkets });
+    });
+
+    return levelAll;
+  };
 
   const sortLevelMiddle = (array = []) => {
-    return array.filter(el => (el.category === 'Бытовая техника' || el.category === 'Одежда и обувь') )
-  }
+    return array.filter(el => (el.category === 'Бытовая техника' || el.category === 'Одежда и обувь'));
+  };
 
   const handleFileChange = (event) => {
-    const file = event.target.files[0];
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    setJsonData(null);
+  };
+
+  const parseFile = () => {
     if (!file) return;
 
     const reader = new FileReader();
@@ -81,53 +87,44 @@ export const Parser = () => {
 
       const jsonLevelTop = sortLevelTop(XLSX.utils.sheet_to_json(levelTop));
       const jsonLevelAll = sortLevelAll(XLSX.utils.sheet_to_json(levelAll));
-      const jsonLevelMiddle = sortLevelMiddle(jsonLevelAll)
+      const jsonLevelMiddle = sortLevelMiddle(jsonLevelAll);
 
-      setJsonData({...markets, levelTop: jsonLevelTop, levelMiddle: jsonLevelMiddle, levelAll: jsonLevelAll});
+      setJsonData({ ...markets, levelTop: jsonLevelTop, levelMiddle: jsonLevelMiddle, levelAll: jsonLevelAll });
     };
 
     reader.readAsArrayBuffer(file);
   };
 
   const onCopyClickHandle = () => {
-    navigator.clipboard.writeText(JSON.stringify(jsonData, null, 2))
-  }
+    navigator.clipboard.writeText(JSON.stringify(jsonData, null, 2));
+  };
 
   const handleOsSwitch = (type = 'web') => {
-    setOsType(type)
-    setJsonData(null)
-  }
+    setOsType(type);
+    setJsonData(null);
+  };
 
   const onDownloadClickHandle = () => {
-    const fileName = "markets.json"
-    const json = JSON.stringify(jsonData, null, 2) 
-    const blob = new Blob([json], { type: 'application/json' })
-    const href = URL.createObjectURL(blob)
-  
-    const link = document.createElement('a')
-    link.href = href
-    link.download = fileName
-    document.body.appendChild(link)
-    link.click()
-  
-    document.body.removeChild(link)
-    URL.revokeObjectURL(href)
-  }
+    const fileName = 'markets.json';
+    const json = JSON.stringify(jsonData, null, 2);
+    const blob = new Blob([json], { type: 'application/json' });
+    const href = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = href;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(href);
+  };
 
   return (
     <div>
       <h1>BNPL partners Excel to JSON Parser</h1>
-      <div className="switcher">
-        <button 
-          className={osType === 'web' ? 'switcher_btn switcher_btn-active' : 'switcher_btn'}
-          onClick={() => handleOsSwitch('web')}
-          >Web</button>
-        <button 
-          className={osType === 'mobile' ? 'switcher_btn switcher_btn-active' : 'switcher_btn'}
-          onClick={() => handleOsSwitch('mobile')}
-          >Mobile</button>
-      </div>
       <div className="file-upload-container">
+        1. Выберите Excel файл
         <label htmlFor="file-upload" className="custom-file-upload">
           Выбрать файл
         </label>
@@ -138,29 +135,47 @@ export const Parser = () => {
           accept=".xlsx, .xls" 
           onChange={handleFileChange} 
         />
-        {jsonData && 
-        <div className="buttons">
-          <button 
-            className="save_json_btn"
-            onClick={onCopyClickHandle}
-          >
-            Скопировать JSON
-            </button>
-            <button 
-            className="save_json_btn"
-            onClick={onDownloadClickHandle}
-          >
-            Скачать JSON
-          </button>
-        </div>
-        }
       </div>
+
+      {file && (
+        <>
+        2. Выберите тип приложения
+          <div className="switcher">
+            <button
+              className={osType === 'web' ? 'switcher_btn switcher_btn-active' : 'switcher_btn'}
+              onClick={() => handleOsSwitch('web')}
+            >
+              Web
+            </button>
+            <button
+              className={osType === 'mobile' ? 'switcher_btn switcher_btn-active' : 'switcher_btn'}
+              onClick={() => handleOsSwitch('mobile')}
+            >
+              Mobile
+            </button>
+          </div>
+          <button className="parse-btn" onClick={parseFile}>
+            Создать JSON
+          </button>
+        </>
+      )}
+
       {jsonData && (
-        <div>
-          <h2>Parsed JSON Data:</h2>
-          <pre>{JSON.stringify(jsonData, null, 2)}</pre>
-        </div>
+        <>
+          <div className="buttons">
+            <button className="save_json_btn" onClick={onCopyClickHandle}>
+              Скопировать JSON
+            </button>
+            <button className="save_json_btn" onClick={onDownloadClickHandle}>
+              Скачать JSON
+            </button>
+          </div>
+          <div>
+            <h2>Parsed JSON Data:</h2>
+            <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+          </div>
+        </>
       )}
     </div>
   );
-}
+};
